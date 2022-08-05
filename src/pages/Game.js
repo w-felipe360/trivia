f06d98a8eae98e6fa7d2f9b6f712269d031393e5
Jean Freitas <jeanjfe94@gmail.com>
2022-08-05 17:25:47 -0300

    feito o requisito 2

---
+++ src/pages/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        Página do Jogo
+      </div>
+    );
+  }
+}
+
+export default Game;
---
+++ src/services/requestAPI.js
@@ -0,0 +1,20 @@
+const GET_TOKEN = 'https://opentdb.com/api_token.php?command=request';
+
+export const setLocalStorage = (response) => {
+  localStorage.setItem('token', response);
+};
+
+export const getToken = async () => {
+  const response = await fetch(GET_TOKEN);
+  const json = await response.json();
+
+  return response.ok ? Promise.resolve(json) : Promise.reject(json);
+};
+
+export const getQuestions = async () => {
+  const responseToken = await getToken();
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${responseToken.token}`);
+  const json = await response.json();
+
+  return response.ok ? Promise.resolve(json) : Promise.reject(json);
+};
