601b77c7fc2f907660e2c304af8d1b3f603a3774
Rubio <paulocezarrubio@gmail.com>
2022-08-10 16:07:54 -0300

    req18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Ranking extends React.Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <>
         <Header />
         <section>
           <h1 data-testid="ranking-title"> Ranking </h1>
+          <button
+            data-testid="btn-go-home"
+            type="button"
+            onClick={ this.handleClick }
+          >
+            Vai Pra Casa
+
+          </button>
         </section>
       </>
     );
@@ -15,3 +29,9 @@ class Ranking extends React.Component {
 }
 
 export default Ranking;
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
